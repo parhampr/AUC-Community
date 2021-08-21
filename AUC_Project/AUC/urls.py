dff2e8a6039b3f1f6c309793b9c3154975d88e31
Mr_x_master <kamal20012011@hotmail.com>
2021-08-21 20:10:29 +0400

    REMENCING THE PREVIOUS COMMIT

--- AUC_Project/AUC/urls.py
+++ AUC_Project/AUC/urls.py
@@ -5,17 +5,21 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 
-app_name = "AUC"
 urlpatterns = [
     path('', views.testing, name='testing'),
     path("accounts/register/", views.register, name="register"),
     path("accounts/login/", views.login_view, name="login"),
     path("accounts/logout", views.logout_view, name="logout"),
     path("accounts/login/usercheck", views.userCheck, name='userAvailability'),
-    path("reset_password/", auth_views.PasswordResetView.as_view(template_name="AUC/AuthenticateProfile.html", html_email_template_name="AUC/Auth/acc_pass_reset_email.html", subject_template_name="AUC/Auth/password_reset_custom_subject.txt"), name="reset_password"),
-    path("reset_password_sent/", auth_views.PasswordResetDoneView.as_view(template_name="AUC/AuthenticateProfile.html"), name="password_reset_done"),
-    path("accounts/user/password_reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(template_name="AUC/AuthenticateProfile.html"), name="password_reset_confirm"),
-    path("reset_password_complete/", views.password_reset_successfully, name="password_reset_complete"),
+    path("reset_password/", auth_views.PasswordResetView.as_view(template_name="AUC/AuthenticateProfile.html",
+         html_email_template_name="AUC/Auth/acc_pass_reset_email.html", subject_template_name="AUC/Auth/password_reset_custom_subject.txt"), name="reset_password"),
+    path("reset_password_sent/", auth_views.PasswordResetDoneView.as_view(
+        template_name="AUC/AuthenticateProfile.html"), name="password_reset_done"),
+    path("accounts/user/password_reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(
+        template_name="AUC/AuthenticateProfile.html"), name="password_reset_confirm"),
+    path("reset_password_complete/", views.password_reset_successfully,
+         name="password_reset_complete"),
     path("accounts/user/confirm_email", views.Confirm_Email, name='activate'),
-    path("accounts/user/send_verification/", views.ReVerificationEmail, name='send_verify')
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path("accounts/user/send_verification/",
+         views.ReVerificationEmail, name='send_verify')
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
